bf0222ce5a68ea0aaa2e53663d66bfde68ad69f8
William Griffiths <willjgriff@gmail.com>
2017-10-02 13:48:57 +0100

    Updated truffle config to allow for large contract creation.

--- truffle.js
+++ truffle.js
@@ -5,12 +5,14 @@ module.exports = {
             host: "localhost",
             port: 8545,
             network_id: "*" // Match any network id
+            network_id: "*", // Match any network id
+            gas: 5000000
         },
         testnet: {
             host: "localhost",
             port: 8547,
             network_id: 4,
-            gas: 4000000,
+            gas: 6000000,
             gasPrice: 21000000111
         }
     }
