7da988596cd3a7c44952d2bef9a2a01cde9cb278
William Griffiths <willjgriff@gmail.com>
2017-10-06 12:52:37 +0100

    Updated uPort app ID.

--- src/util/connectors.js
+++ src/util/connectors.js
@@ -1,9 +1,17 @@
 import { Connect, SimpleSigner } from 'uport-connect'
 
-export const uport = new Connect('Individuality Attester', {
-  clientId: '2otwkJqvG8m5dt9RevQGiHXGh3Nez1i7KDW',
+export const uport = new Connect('Hive Commons', {
+  clientId: '2or25VKPtkdhvFMwbh79UKKP6XhN48ffr1Y',
   network: 'rinkeby',
-  signer: SimpleSigner('411a0c0a5f8725027ab2691b63e58dff9d9c9eb55ef29bc63c989b1d7d435073')
+  signer: SimpleSigner('ff0457c59e78c321924f5d86748213daeb7b17622b06e31bfa76585e10f1bbb6')
 })
+
+// Attestation uPort app.
+// export const uport = new Connect('Hive Commons Individuality Attester', {
+//     clientId: '2oghfQT26sXXsgTKPhFccWq3inDVWwH4TUX',
+//     network: 'rinkeby',
+//     signer: SimpleSigner('c0fee934f5213c571e0014c25a7efab062edda51951e3cc2047495046b91c1ca')
+// })
+
 
 export const web3 = uport.getWeb3()
